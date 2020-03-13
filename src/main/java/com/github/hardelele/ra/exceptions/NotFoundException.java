f6aac6154113b7642aefb9da5a3d0f200335f1f2
Hardelele <Hardelele@yahoo.com>
2020-03-14 02:30:21 +0300

    Change handlers directory

--- src/main/java/com/github/hardelele/ra/exceptions/NotFoundException.java
+++ src/main/java/com/github/hardelele/ra/exceptions/NotFoundException.java
@@ -1,14 +1,16 @@
 package com.github.hardelele.ra.exceptions;
 
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+@Data
 public class NotFoundException extends RuntimeException {
 
-    private HttpStatus code;
+    private HttpStatus httpStatus;
     private String message;
 
-    public NotFoundException(String message, HttpStatus code) {
-        this.code = code;
+    public NotFoundException(String message, HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
         this.message = message;
     }
 }
