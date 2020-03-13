f4c62a3f6de3d57cd00e2cf09d7762f0ff314b46
Hardelele <Hardelele@yahoo.com>
2020-03-13 21:09:29 +0300

    Init empty project

---
+++ src/main/java/com/github/hardelele/ra/RecipeApplication.java
@@ -0,0 +1,12 @@
+package com.github.hardelele.ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class);
+    }
+}
