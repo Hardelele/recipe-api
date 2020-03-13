05cdde0f1ec352ce90b9b4dea02a7246e59d8b15
Hardelele <Hardelele@yahoo.com>
2020-03-14 00:36:17 +0300

    Add JpaRepository IngredientRepository

---
+++ src/main/java/com/github/hardelele/ra/repositories/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.github.hardelele.ra.repositories;
+
+import com.github.hardelele.ra.models.entities.IngredientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<IngredientEntity, UUID> {
+}
