25eca8097e83b76d0363ae5ab08f8457af334de5
Hardelele <Hardelele@yahoo.com>
2020-03-14 00:37:04 +0300

    Add IngredientService with get and create methods

---
+++ src/main/java/com/github/hardelele/ra/services/IngredientService.java
@@ -0,0 +1,61 @@
+package com.github.hardelele.ra.services;
+
+import com.github.hardelele.ra.exceptions.NotFoundException;
+import com.github.hardelele.ra.models.entities.IngredientEntity;
+import com.github.hardelele.ra.models.forms.IngredientForm;
+import com.github.hardelele.ra.models.transfers.IngredientTransfer;
+import com.github.hardelele.ra.repositories.IngredientRepository;
+import com.github.hardelele.ra.utils.enums.Status;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class IngredientService {
+
+    private final IngredientRepository ingredientRepository;
+
+    private final Mapper mapper;
+
+    private final Date date = new Date();;
+
+    @Autowired
+    public IngredientService(IngredientRepository ingredientRepository, Mapper mapper) {
+        this.ingredientRepository = ingredientRepository;
+        this.mapper = mapper;
+    }
+
+    public List<IngredientTransfer> getAllIngredients() {
+        return ingredientRepository.findAll().stream()
+                .map(entity -> mapper.map(entity, IngredientTransfer.class))
+                .collect(Collectors.toList());
+    }
+
+    public IngredientTransfer getIngredient(UUID id) {
+        IngredientEntity entity = ingredientRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Not found ingredient by id:" + id, HttpStatus.NOT_FOUND));
+        return mapper.map(entity, IngredientTransfer.class);
+    }
+
+    public IngredientTransfer createIngredient(IngredientForm ingredientForm) {
+
+        IngredientEntity ingredientToSave = IngredientEntity.builder()
+                .id(UUID.randomUUID())
+                .name(ingredientForm.getName())
+                .status(Status.ACTIVE)
+                .timestamp(new Timestamp(date.getTime()))
+                .build();
+
+        ingredientToSave = ingredientRepository.save(ingredientToSave);
+        return mapper.map(ingredientToSave, IngredientTransfer.class);
+    }
+}
