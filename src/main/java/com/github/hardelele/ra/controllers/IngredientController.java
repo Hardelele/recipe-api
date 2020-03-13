edb91b257ed713cd80cd8b652b1668cefcc6b9d2
Hardelele <Hardelele@yahoo.com>
2020-03-14 00:31:59 +0300

    Add Post and Get mapping

--- src/main/java/com/github/hardelele/ra/controllers/IngredientController.java
+++ src/main/java/com/github/hardelele/ra/controllers/IngredientController.java
@@ -1,19 +1,37 @@
 package com.github.hardelele.ra.controllers;
 
-import com.github.hardelele.ra.models.transfers.IngredientTransfer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.hardelele.ra.objects.forms.IngredientForm;
+import com.github.hardelele.ra.objects.models.IngredientModel;
+import com.github.hardelele.ra.objects.transfers.IngredientTransfer;
+import com.github.hardelele.ra.services.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class IngredientController {
 
+    private final IngredientService ingredientService;
+
+    @Autowired
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
     @GetMapping("/ingredients")
     public List<IngredientTransfer> getAllIngredients() {
-        List<IngredientTransfer> ingredients = new ArrayList<>();
-        ingredients.add(IngredientTransfer.builder().build());
-        return ingredients;
+        return ingredientService.getAllIngredients();
+    }
+
+    @GetMapping("/ingredients/{id}")
+    public IngredientTransfer getIngredient(@PathVariable UUID id) {
+        return ingredientService.getIngredient(id);
+    }
+
+    @PostMapping("/ingredients")
+    public IngredientTransfer getIngredient(@RequestBody IngredientForm ingredient) {
+        return ingredientService.createIngredient(ingredient);
     }
 }
