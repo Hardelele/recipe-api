6590936e327cafdef8f229971c5216919aae8e25
Hardelele <Hardelele@yahoo.com>
2020-03-14 00:31:22 +0300

    Fix SwaggerConfig by @Configuration

--- src/main/java/com/github/hardelele/ra/configs/SwaggerConfig.java
+++ src/main/java/com/github/hardelele/ra/configs/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.github.hardelele.ra.configs;
 
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 }
