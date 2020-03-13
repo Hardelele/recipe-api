4addb1a847156002700099c2c69cccfc57d7ed05
Hardelele <Hardelele@yahoo.com>
2020-03-14 02:33:15 +0300

    Add id field

--- src/main/java/com/github/hardelele/ra/models/transfers/IngredientTransfer.java
+++ src/main/java/com/github/hardelele/ra/models/transfers/IngredientTransfer.java
@@ -1,16 +1,15 @@
 package com.github.hardelele.ra.models.transfers;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class IngredientTransfer {
 
+    private String id;
     private String name;
     private long timestamp;
 }
