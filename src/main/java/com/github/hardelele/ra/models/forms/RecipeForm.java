dfa7d3c444aadeaa5a7ed1365a5a69e82b00d45b
Hardelele <Hardelele@yahoo.com>
2020-03-14 23:40:05 +0300

    Add RecipeForm

---
+++ src/main/java/com/github/hardelele/ra/models/forms/RecipeForm.java
@@ -0,0 +1,26 @@
+package com.github.hardelele.ra.models.forms;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.hardelele.ra.models.entities.IngredientEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RecipeForm {
+
+    private String name;
+
+    private String description;
+
+    @JsonProperty(value = "cooking_in_milliseconds")
+    private long cookingMilliseconds;
+
+    private Set<IngredientForm> ingredients;
+}
