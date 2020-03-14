00dd3721f26bbd366cb510a4726397629f1038c7
Hardelele <Hardelele@yahoo.com>
2020-03-14 23:42:14 +0300

    Add RecipeTransfer

---
+++ src/main/java/com/github/hardelele/ra/models/transfers/RecipeTransfer.java
@@ -0,0 +1,28 @@
+package com.github.hardelele.ra.models.transfers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecipeTransfer {
+
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    @JsonProperty(value = "cooking_in_milliseconds")
+    private long cookingMilliseconds;
+
+    private Set<IngredientTransfer> ingredients;
+
+    private Timestamp timestamp;
+}
