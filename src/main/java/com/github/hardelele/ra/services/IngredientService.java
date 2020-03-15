79666042713076c34eaf5d50fdb6d9bbcb82de43
Hardelele <Hardelele@yahoo.com>
2020-03-15 05:50:06 +0300

    Split logic into database and cache services

--- src/main/java/com/github/hardelele/ra/services/IngredientService.java
+++ src/main/java/com/github/hardelele/ra/services/IngredientService.java
@@ -4,58 +4,59 @@ import com.github.hardelele.ra.exceptions.AlreadyExistException;
 import com.github.hardelele.ra.exceptions.NotFoundException;
 import com.github.hardelele.ra.models.entities.IngredientEntity;
 import com.github.hardelele.ra.models.forms.IngredientForm;
-import com.github.hardelele.ra.repositories.IngredientRepository;
-import com.github.hardelele.ra.services.cache.DoubleKeyCache;
-import com.github.hardelele.ra.services.cache.keys.CacheKey;
-import com.github.hardelele.ra.utils.enums.Status;
+import com.github.hardelele.ra.services.cache.IngredientCacheService;
+import com.github.hardelele.ra.services.database.IngredientDatabaseService;
+import com.github.hardelele.ra.utils.cache.CacheKey;
+import com.github.hardelele.ra.utils.mapping.IngredientMapper;
 import org.dozer.Mapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
-@Transactional
 public class IngredientService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(IngredientService.class);
+    private final IngredientCacheService ingredientCacheService;
 
-    private final IngredientRepository ingredientRepository;
+    private final IngredientDatabaseService ingredientDatabaseService;
 
-    private final Mapper mapper;
-
-    private final DoubleKeyCache<IngredientEntity> ingredientsCache = new DoubleKeyCache<>();
+    private final IngredientMapper ingredientMapper;
 
     @Autowired
-    public IngredientService(IngredientRepository ingredientRepository, Mapper mapper) {
-        this.ingredientRepository = ingredientRepository;
-        this.mapper = mapper;
+    public IngredientService(IngredientCacheService ingredientCacheService,
+                             IngredientDatabaseService ingredientDatabaseService,
+                             IngredientMapper ingredientMapper) {
+        this.ingredientCacheService = ingredientCacheService;
+        this.ingredientDatabaseService = ingredientDatabaseService;
+        this.ingredientMapper = ingredientMapper;
     }
 
     public List<IngredientEntity> getAllIngredients() {
-        return ingredientRepository.findAll();
+        return ingredientDatabaseService.getAllIngredients().stream()
+                .map(ingredientCacheService::putInCache)
+                .collect(Collectors.toList());
     }
 
     public IngredientEntity getIngredientById(UUID id) {
         try {
-            return pullFormCacheById(id);
+            return ingredientCacheService.pullFormCacheById(id);
         } catch (NullPointerException ignored) {
-            return putInCache(pullFromDatabaseById(id));
+            IngredientEntity ingredientFromDatabase = ingredientDatabaseService.pullFromDatabaseById(id);
+            return ingredientCacheService.putInCache(ingredientFromDatabase);
         }
     }
 
     public IngredientEntity getIngredientByName(String name) {
-        if(!isExistByName(name)) {
-            throw new NotFoundException("ingredient by name:" + name, HttpStatus.NOT_FOUND);
+        try {
+            return ingredientCacheService.pullFormCacheByName(name);
+        } catch (NullPointerException ignored) {
+            IngredientEntity ingredientFromDatabase = ingredientDatabaseService.pullFromDatabaseByName(name);
+            return ingredientCacheService.putInCache(ingredientFromDatabase);
         }
-        return ingredientRepository.findByName(name);
     }
 
     public IngredientEntity createIngredient(IngredientForm ingredientFromForm) {
@@ -63,77 +64,30 @@ public class IngredientService {
         if (isExistByName(name)) {
             throw new AlreadyExistException("Ingredient by name: " + name, HttpStatus.BAD_REQUEST);
         }
-        return ingredientRepository.save(formToEntity(ingredientFromForm));
+        return putInDatabaseAndCache(ingredientMapper.toEntity(ingredientFromForm));
     }
 
     public IngredientEntity updateIngredient(UUID id, IngredientForm ingredientForm) {
-        IngredientEntity ingredientEntity;
-        try {
-            ingredientEntity = pullFormCacheById(id);
-        } catch (NullPointerException ignored) {
-            ingredientEntity = pullFromDatabaseById(id);
-        }
+        IngredientEntity ingredientEntity = getIngredientById(id);
         ingredientEntity.setName(ingredientForm.getName());
-        return putInDatabase(ingredientEntity);
-    }
-
-    public void deleteAllIngredients() {
-        ingredientRepository.deleteAll();
+        return putInDatabaseAndCache(ingredientEntity);
     }
 
     public void deleteIngredient(UUID id) {
-        ingredientRepository.deleteById(id);
+        CacheKey cacheKey = ingredientDatabaseService.deleteIngredient(id);
+        ingredientCacheService.deleteFromCache(cacheKey);
     }
 
-    private IngredientEntity pullFromDatabaseById(UUID id) {
-
-        LOGGER.info("Pulling ingredient entity form database by id: {}", id.toString());
-        IngredientEntity ingredientEntity = ingredientRepository.findById(id)
-                .orElseThrow(() -> {
-                    LOGGER.info("Can not find nothing in database by id: {}", id.toString());
-                    throw new NotFoundException("ingredient by id: " + id, HttpStatus.NOT_FOUND);
-                });
-
-        LOGGER.info("Got entity from database by id: {}, cache = {}", id.toString(), ingredientEntity);
-        return ingredientEntity;
+    public void deleteAllIngredients() {
+        ingredientDatabaseService.deleteAllIngredients();
+        ingredientCacheService.cleanUp();
     }
 
-    private IngredientEntity pullFormCacheById(UUID id) {
-
-        LOGGER.info("Pulling ingredient entity form cache by id: {}", id.toString());
-        IngredientEntity ingredientFromCache = ingredientsCache.getIfPresent(id);
-
-        if (ingredientFromCache == null) {
-            LOGGER.info("Got empty cache by id: {}", id.toString());
-            throw new NullPointerException();
-        }
-
-        LOGGER.info("Got entity form cache by id: {}, cache = {}", id.toString(), ingredientFromCache);
-        return ingredientFromCache;
+    private IngredientEntity putInDatabaseAndCache(IngredientEntity ingredientToSave) {
+        return ingredientCacheService.putInCache(ingredientDatabaseService.putInDatabase(ingredientToSave));
     }
 
-    private IngredientEntity putInCache(IngredientEntity ingredientEntity) {
-        CacheKey cacheKey = new CacheKey(ingredientEntity.getId(),ingredientEntity.getName());
-        LOGGER.info("Putting ingredient entity in cache: {}", ingredientEntity);
-        ingredientsCache.put(cacheKey, ingredientEntity);
-        return ingredientEntity;
-    }
-
-    private IngredientEntity putInDatabase(IngredientEntity ingredientToSave) {
-        ingredientRepository.save(ingredientToSave);
-        return putInCache(ingredientToSave);
-    }
-
-    public IngredientEntity formToEntity(IngredientForm ingredientForm) {
-        Date date = new Date();
-        IngredientEntity entity = mapper.map(ingredientForm, IngredientEntity.class);
-        entity.setId(UUID.randomUUID());
-        entity.setTimestamp(new Timestamp(date.getTime()));
-        entity.setStatus(Status.ACTIVE);
-        return entity;
-    }
-
-    public boolean isExistByName(String name){
-        return ingredientRepository.existsByName(name);
+    public boolean isExistByName(String name) {
+        return ingredientDatabaseService.isExistByName(name);
     }
 }
