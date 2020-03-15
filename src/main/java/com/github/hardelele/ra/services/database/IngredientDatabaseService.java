a62cca63aa4d1753f88aff7901dbe91105b4c206
Hardelele <Hardelele@yahoo.com>
2020-03-15 07:22:42 +0300

    deleteAllIngredients set status as DELETED

--- src/main/java/com/github/hardelele/ra/services/database/IngredientDatabaseService.java
+++ src/main/java/com/github/hardelele/ra/services/database/IngredientDatabaseService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -34,7 +35,11 @@ public class IngredientDatabaseService {
     }
 
     public void deleteAllIngredients() {
-        ingredientRepository.deleteAll();
+        ingredientRepository.findAll()
+                .forEach(ingredientEntity -> {
+                    UUID id = ingredientEntity.getId();
+                    deleteIngredient(id);
+                });
     }
 
     public CacheKey deleteIngredient(UUID id) {
