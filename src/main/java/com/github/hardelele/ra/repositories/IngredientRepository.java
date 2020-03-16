95678bb6c52da012992eeaa6ac5d075cdee2b09b
Hardelele <Hardelele@yahoo.com>
2020-03-16 04:30:20 +0300

    Add findAllByRecipe_Id

--- src/main/java/com/github/hardelele/ra/repositories/IngredientRepository.java
+++ src/main/java/com/github/hardelele/ra/repositories/IngredientRepository.java
@@ -4,10 +4,12 @@ import com.github.hardelele.ra.models.entities.IngredientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<IngredientEntity, UUID> {
     boolean existsByName(String name);
     IngredientEntity findByName(String name);
+    List<IngredientEntity> findAllByRecipe_Id(UUID id);
 }
